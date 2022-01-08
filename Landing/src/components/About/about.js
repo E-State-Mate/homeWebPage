1842c7e60b2096195aa6f9f6554be4d1f5a2d416
ewideman <92744786+ewideman@users.noreply.github.com>
2022-01-07 19:28:17 -0500

    v3

--- Landing/src/components/About/about.js
+++ Landing/src/components/About/about.js
@@ -11,7 +11,7 @@ class About extends Component {
         super(props);
         this.state = {
             data : [
-                { icon : "mdi mdi-monitor", title : "Web design", desc : "Aliquam tempor an tidunt liberonon feugiat quam risu tortor." },
+                { icon : "mdi mdi-monitor", title : "Web design", desc : "Hello world" },
                 { icon : "mdi mdi-nfc-variant", title : "Development", desc : "Aliquam tempor an tidunt liberonon feugiat quam risu tortor.", isLight : true },
                 { icon : "mdi mdi-lightbulb-on-outline", title : "Branding", desc : "Aliquam tempor an tidunt liberonon feugiat quam risu tortor.-" },
             ]
