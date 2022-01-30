2cb5004aa9bf72006ea4493aa2466e5755f61a00
Sylvia Jung <92225958+syltiva@users.noreply.github.com>
2022-01-29 19:02:43 -0500

    class to function component
    started converting ModalSection and ./Index1/section.js to function component.
    
    section.js imports ModalSection

--- Landing/src/components/common/ModalSection.js
+++ Landing/src/components/common/ModalSection.js
@@ -1,29 +1,31 @@
-import React, { Component } from 'react';
+import React,  { useState } from 'react';
 
 //Importing Modal
 import ModalVideo from 'react-modal-video';
 import '../../../node_modules/react-modal-video/scss/modal-video.scss';
 
-class ModalSection extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            isOpen: false
-        };
-        this.openModal.bind(this)
-    }
+function ModalSection() {
+    const [isOpen, setIsOpen] = useState(false)
+    // constructor(props) {
+    //     super(props);
+    //     this.state = {
+    //         isOpen: false
+    //     };
+    //     this.openModal.bind(this) // In class based components, 'bind' binds the methods you pass to other components  
+    // }
     
-    openModal() {
-        this.setState({ isOpen: true })
-    }
+    const openModal = () => setIsOpen(!isOpen) 
+    // openModal() {
+    //     this.setState({ isOpen: true })
+    // }
 
-    render() {
-        return (
-            <React.Fragment>
-                <ModalVideo channel={this.props.channel} isOpen={this.state.isOpen} videoId={this.props.videoId} onClose={() => this.setState({isOpen: false})} />
-            </React.Fragment>
-        );
-    }
+
+    return (
+        <React.Fragment>
+            <ModalVideo channel={this.props.channel} isOpen={this.state.isOpen} videoId={this.props.videoId} onClose={() => this.setState({isOpen: false})} />
+        </React.Fragment>
+    );
+    
 }
 
 export default ModalSection;
--- Landing/src/pages/Index2/section.js
+++ Landing/src/pages/Index2/section.js
@@ -1,61 +1,63 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Link } from "react-router-dom";
 import { Container, Row, Col } from "reactstrap";
+import { useState } from 'react';
 
 //Importing Modal
 import ModalSection from '../../components/common/ModalSection';
 
-class Section extends Component {
-    constructor() {
-        super();
-        this.state = {
-            isOpen: false
-        }
-        this.callModal.bind(this)
-    }
+function Section() {
+    const [isOpen, setIsOpen]= useState(false)
+    
+    // constructor() {
+    //     super();
+    //     this.state = {
+    //         isOpen: false
+    //     }
+    //     this.callModal.bind(this) // what is this for?
+    // }
 
-    callModal = () => {
-        this.refs.child.openModal();
+    const callModal = () => {
+        this.refs.child.openModal(); // TODO: figure out how to change to function 
     }
     
-    render() {
-        return (
-            <React.Fragment>
-            <section className="bg-home home-half" id="home">
-                <div className="bg-overlay"></div>
-                <div className="home-center">
-                    <div className="home-desc-center">
-                        <Container>
-                            <Row className="justify-content-center">
-                                <Col md={12}>
-                                    <div className="home-content text-white">
-                                        <div className="watch-video mt-5">
-                                        <Link onClick={this.callModal} to="#" className="video-play-icon-trigger text-white">
-                                                <i className="mdi mdi-play play-icon-circle play play-icon f-30"></i>
-                                            </Link>
-                                        </div>
-                                        <h5 className="sub-title mt-4 text-white pt-2 text-uppercase">Xeril Template</h5>
-                                        <h1 className="title mt-4 text-white text-uppercase">We're Giving Design <br/> Solution & Idea.</h1>
-                                        <div className="pt-4 mt-1">
-                                            <Link to="#" className="btn btn-outline-white mt-2 mr-3">Get Started</Link>
-                                            <Link to="#" className="btn btn-primary ml-1 mt-2">Purchase Now</Link>
-                                        </div>
+    
+    return (
+        <React.Fragment>
+        <section className="bg-home home-half" id="home">
+            <div className="bg-overlay"></div>
+            <div className="home-center">
+                <div className="home-desc-center">
+                    <Container>
+                        <Row className="justify-content-center">
+                            <Col md={12}>
+                                <div className="home-content text-white">
+                                    <div className="watch-video mt-5">
+                                    <Link onClick={this.callModal} to="#" className="video-play-icon-trigger text-white">
+                                            <i className="mdi mdi-play play-icon-circle play play-icon f-30"></i>
+                                        </Link>
                                     </div>
-        
-        
-                                    
-                                    
-                                </Col>
-                            </Row>
-                        </Container>
-                        {/* Render ModalSection Component for Modal */}
-                        <ModalSection ref="child" channel='vimeo' videoId='99025203' />
-                    </div>
+                                    <h5 className="sub-title mt-4 text-white pt-2 text-uppercase">E-StateMates Services LLC</h5>
+                                    <h1 className="title mt-4 text-white text-uppercase">Statement <br/>Here</h1>
+                                    <div className="pt-4 mt-1">
+                                        <Link to="#" className="btn btn-outline-white mt-2 mr-3">Get Started</Link>
+                                        <Link to="#" className="btn btn-primary ml-1 mt-2">Purchase Now</Link>
+                                    </div>
+                                </div>
+    
+    
+                                
+                                
+                            </Col>
+                        </Row>
+                    </Container>
+                    {/* Render ModalSection Component for Modal */}
+                    <ModalSection ref="child" channel='vimeo' videoId='99025203' />
+                    {/* Modal section connected to js file, also in class component format */}
                 </div>
-            </section>
-            </React.Fragment>
-        );
-    }
-}
+            </div>
+        </section>
+        </React.Fragment>
+    )}
 
 export default Section;
