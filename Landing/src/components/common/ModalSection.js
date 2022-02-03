f8a325acb20771f6f7f08949d3528a52882b941a
Sylvia Jung <92225958+syltiva@users.noreply.github.com>
2022-02-03 12:05:47 -0500

    splash section and process section
    both sections are now converted to function components.

--- Landing/src/components/common/ModalSection.js
+++ Landing/src/components/common/ModalSection.js
@@ -1,28 +1,17 @@
 import React,  { useState } from 'react';
 
 //Importing Modal
-import ModalVideo from 'react-modal-video';
+// import ModalVideo from 'react-modal-video';
 import '../../../node_modules/react-modal-video/scss/modal-video.scss';
 
-function ModalSection(props) {
-    const [isOpen, setIsOpen] = useState(false)
-    // constructor(props) {
-    //     super(props);
-    //     this.state = {
-    //         isOpen: false
-    //     };
-    //     this.openModal.bind(this) // In class based components, 'bind' binds the methods you pass to other components  
-    // }
-    
-    const openModal = () => setIsOpen(!isOpen) 
-    // openModal() {
-    //     this.setState({ isOpen: true })
-    // }
+const ModalSection = (props) => {
+    // const [isOpen, setIsOpen] = useState(false)
 
+    // const openModal = () => setIsOpen(!isOpen) 
 
     return (
         <React.Fragment>
-            <ModalVideo channel={channel} isOpen={isOpen} videoId={videoId} onClose={() => {setIsOpen(false)}} />
+            {/* <ModalVideo channel={channel} isOpen={isOpen} videoId={videoId} onClose={() => {setIsOpen(false)}} /> */}
         </React.Fragment>
     );
     
--- Landing/src/pages/Index2/section.js
+++ Landing/src/pages/Index2/section.js
@@ -4,9 +4,9 @@ import { Container, Row, Col } from "reactstrap";
 import { useState } from 'react';
 
 //Importing Modal
-import ModalSection from '../../components/common/ModalSection';
+// import ModalSection from '../../components/common/ModalSection';
 
-function Section(props) {
+const Section = () => {
     const [isOpen, setIsOpen]= useState(false)
     
     // constructor() {
@@ -14,12 +14,12 @@ function Section(props) {
     //     this.state = {
     //         isOpen: false
     //     }
-    //     this.callModal.bind(this) // what is this for?
+    //     this.callModal.bind(this)
     // }
 
-    const callModal = () => {
-        this.refs.child.openModal(); // TODO: figure out correct syntax to connect to openModal() in functional component form. 
-    }
+    // const callModal = () => {
+    //     this.refs.child.openModal(); 
+    // }
     
     
     return (
@@ -32,16 +32,16 @@ function Section(props) {
                         <Row className="justify-content-center">
                             <Col md={12}>
                                 <div className="home-content text-white">
-                                    <div className="watch-video mt-5">
-                                    <Link onClick={callModal} to="#" className="video-play-icon-trigger text-white">
+                                    {/* <div className="watch-video mt-5"> */}
+                                    {/* <Link onClick={callModal} to="#" className="video-play-icon-trigger text-white">
                                             <i className="mdi mdi-play play-icon-circle play play-icon f-30"></i>
-                                        </Link>
-                                    </div>
+                                        </Link> */}
+                                    {/* </div> */}
                                     <h5 className="sub-title mt-4 text-white pt-2 text-uppercase">E-StateMates Services LLC</h5>
                                     <h1 className="title mt-4 text-white text-uppercase">Statement <br/>Here</h1>
                                     <div className="pt-4 mt-1">
                                         <Link to="#" className="btn btn-outline-white mt-2 mr-3">Get Started</Link>
-                                        <Link to="#" className="btn btn-primary ml-1 mt-2">Purchase Now</Link>
+                                        <Link to="#" className="btn btn-primary ml-1 mt-2">Get a Quote</Link>
                                     </div>
                                 </div>
     
@@ -52,7 +52,7 @@ function Section(props) {
                         </Row>
                     </Container>
                     {/* Render ModalSection Component for Modal */}
-                    <ModalSection ref="child" channel='vimeo' videoId='99025203' />
+                    {/* <ModalSection ref="child" channel='vimeo' videoId='99025203' /> */}
                     {/* Modal section connected to js file, also in class component format */}
                 </div>
             </div>
